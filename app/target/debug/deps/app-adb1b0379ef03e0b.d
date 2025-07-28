e8a629bf59744d6500b7278b35905cc6beadfafb
tanmay kumar chaurasia <tanmaychaurasia88@gmail.com>
2025-07-28 20:36:44 +0530

    done

---
+++ app/target/debug/deps/app-adb1b0379ef03e0b.d
@@ -0,0 +1,5 @@
+/Users/tanmaykumarchaurasia/Desktop/programming/metaprogramming_macros/app/target/debug/deps/app-adb1b0379ef03e0b.d: src/main.rs
+
+/Users/tanmaykumarchaurasia/Desktop/programming/metaprogramming_macros/app/target/debug/deps/app-adb1b0379ef03e0b: src/main.rs
+
+src/main.rs:
--- serialize_macro/src/lib.rs
+++ serialize_macro/src/lib.rs
@@ -1,14 +1,111 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{Data, DeriveInput, Fields}; // use to parse Rust syntax tree into Abstract syntax tree
+
+#[proc_macro_derive(SerializeNumberStruct)]
+pub fn serialize_number_struct(input: TokenStream) -> TokenStream {
+    let ast: DeriveInput = syn::parse(input).unwrap(); // convert macro input to rust syntax tree
+    let name = &ast.ident; // extract the name of the struct
+
+    let serial_fields = match &ast.data {
+        Data::Struct(data_struct) => {
+            // make sure that we are working with structs only
+            match &data_struct.fields {
+                // this makes sure that inside the struct, there are name fields
+                Fields::Named(fields) => {
+                    let field_serial = fields.name.iter().map(|field| {
+                        // this extracts every field
+                        let field_name = &field.ident;
+                        quote! {
+                            result.extend_from_slice(&self.#field_name.to_be_bytes()); // coverts every field into bytes
+                        }
+                    });
+
+                    quote! {
+                        #(#field_serial)*
+                    }
+                }
+
+                _ => panic!("only named fields are supperted"),
+            }
+        }
+
+        _ => panic!("only structs are supported"),
+    };
+
+    let generated = quote! {
+        impl Serialize for #name {
+            fn serialize(&self) -> Vec<u8> {
+                let mut result = Vec::new();
+                #serial_fields
+                result
+            }
+        }
+    };
+
+    generated.into();
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[proc_macro_derive(DeserializeNumberStruct)]
+pub fn deserialize_number_struct(input: TokenStream) -> TokenStream {
+    let ast: DeriveInput = syn::parse(input).unwrap(); // macro input to syntax tree
+    let name = &ast.ident;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let (deserialize_fields, field_assignments, total_size) = match &ast.data {
+        Data::Struct(data_struct) => match &data_struct.fields {
+            Fields::Named(fields) => {
+                let mut offset: usize = 0;
+                let mut field_deserial = Vec::new();
+                let mut field_assignments = Vec::new();
+
+                for field in &fields.named {
+                    let field_name = &field.ident;
+                    let feild_size = 4;
+                    let start_offset = offset;
+                    let end_offset = offset + feild_size;
+
+                    field_deserial.push(quote! {
+                            let #field_name = {
+                                let bytes: [u8; 4] = base[#start_offset..#end_offset].try_into().map_err(|_| Error)?;
+                                i32::from_be_bytes(bytes)
+                            };
+                        });
+
+                    field_assignments.push(quote! {
+                        #field_name
+                    });
+
+                    offset += feild_size;
+                }
+
+                (field_deserial, field_assignments, offset);
+            }
+
+            _ => panic!("only named fields are supported"),
+        },
+
+        _ => panic!("only structs are supported"),
+    };
+
+    // Generate full impl block for Deserialize
+    let generated = quote! {
+        impl Deserialize for #name {
+            fn deserialize(base: &[u8]) -> Result<Self, Error> {
+                // Check that enough bytes were passed
+                if base.len() < #total_size {
+                    return Err(Error);
+                }
+
+                // Field deserialization code
+                #(#deserialize_fields)*
+
+                // Construct struct using deserialized values
+                Ok(#name {
+                    #(#field_assignments,)*
+                })
+            }
+        }
+    };
+
+    generated.into()
 }
