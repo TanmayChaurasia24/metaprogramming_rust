23bec6abaaf7525e4410a2196c358f378072d07e
tanmay kumar chaurasia <tanmaychaurasia88@gmail.com>
2025-07-28 19:32:40 +0530

    written traits for CDM

--- serialize_macro_trait/src/lib.rs
+++ serialize_macro_trait/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::fmt::Error;
+
+pub trait Serialize {
+	fn serialize(&self) -> Vec<u8>;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub trait Deserialize: Sized {
+	fn deserialize(base: &[u8]) -> Result<Self, Error>;
 }
